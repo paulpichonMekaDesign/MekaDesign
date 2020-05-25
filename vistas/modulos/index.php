fa5a8076a0c7b19b8710073e8997d761de396540
Paúl Pichón <paul@mekadesign.mx>
2020-05-25 11:24:03 -0500

    Creacion del menu estilo hamburguesa solo falta definir algunos detalles como si va a llevar un color de background

--- vistas/modulos/index.php
+++ vistas/modulos/index.php
@@ -12,7 +12,7 @@ $url = Rutas::rutasArchivosMdl();
 	<!-- Behavioral Meta Data -->
 	<meta name="apple-mobile-web-app-capable" content="yes">
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-	<!-- BOOTSTRAP -->
+	<!-- BOOTSTRAP 4-->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	<!-- ESTILOS PROPIOS -->
 	<link rel="stylesheet" type="text/css" href="<?php echo $url; ?>css/estilos.css"/>
@@ -62,7 +62,35 @@ $url = Rutas::rutasArchivosMdl();
 		</ul>
 
 	</div>
+	
+	<!-- Menu hamburguesa -->
+	<div id="header" class="contenedorMenu fixed-top">
+		<div class="container-fluid">
+			<header class="menu d-flex justify-content-between">
+				<div class="logoMekaDesign">
+					<img class="imgLogoMenu" src="vistas/imagenes/1.png" alt="MekaDesign Logo">
+				</div>
+				<div id="menu_on" class="menuHamburguesa d-flex align-items-center">
+					<p class="menuTexto mr-3">Menú</p>
+					<div class="lineasMenuHamburguesa">
+						<hr class="linea1">
+						<hr class="linea2">
+						<hr class="linea3">
+					</div>
+				</div>
+			</header>
+		</div>
+	</div>
+	<!-- Menu hamburguesa -->
 
+	<!-- Menu dezplegado -->
+	<div class="menuDesplegado">
+		<h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia voluptas tempora animi excepturi laboriosam! A unde provident dignissimos quae voluptates saepe, beatae quibusdam nesciunt deserunt nihil velit error placeat molestiae.</h1>
+		<h1>nosotros</h1>
+		<h1>contacto</h1>
+		<h1>servicios</h1>
+	</div>
+	
 	<!-- BOOTSTRAP V.4-->
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
@@ -71,9 +99,45 @@ $url = Rutas::rutasArchivosMdl();
 	<script src="<?php echo $url; ?>js/libraries.min.js"></script>
 	<script src="<?php echo $url; ?>js/jquery.parallax.js"></script>
 
+	<!-- Activacion menu hamburguesa Jquery-->
+	<script type="text/javascript">
+		$( document ).ready(function() {
+			$('#menu_on').click(function(){ // Al hacer click...
+			   $('body').toggleClass('visible_menu'); // Añadimos o eliminamos la clase 'visible_menu' al body
+			 })
+		});
+	</script>
+
 	<script>
+		// EFECTO APARECER/DESAPARECER MENU AL HACER SCROLL
+		//JAVASCRIPT
+		let new_scroll_position = 0;
+		let last_scroll_position;
+		const header = document.getElementById("header");
 
+		window.addEventListener('scroll', function(e) {
+		last_scroll_position = window.scrollY;
 
+		// Scrolling down
+		if (new_scroll_position < last_scroll_position && last_scroll_position > 80) {
+		// header.removeClass('slideDown').addClass('slideUp');
+		header.classList.remove("slideDown");
+		header.classList.add("slideUp");
+
+		// Scrolling up
+		} else if (new_scroll_position > last_scroll_position) {
+		// header.removeClass('slideUp').addClass('slideDown');
+		header.classList.remove("slideUp");
+		header.classList.add("slideDown");
+
+		}
+
+		new_scroll_position = last_scroll_position;
+
+		});
+	</script>
+
+	<script>
 	// jQuery Selections
 	var $html = $('html'),
 			$container = $('#container'),
